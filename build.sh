03cc2b6d3d49af8cd25a5b6a0988d30de9925db7
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-02-17 22:33:13 +0000

    Make DogeConf talk reproducible

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ SHELL="/bin/sh"
 # For some reason, using $0 instead of `sh` makes Hadolint warn about the single-quotes not expanding expressions
 # shellcheck disable=SC2016
 find "$USER_DIR" -type f -name '*.tex' -exec "$SHELL" -c '
+  cp $(dirname "$@")/* .
   output=$(dirname "$@")/$(basename "$@" .tex).pdf
   echo "$@ - $output"
   HOME=$(pwd) SOURCE_DATE_EPOCH=1622905527 pdflatex "$@" || exit 1
